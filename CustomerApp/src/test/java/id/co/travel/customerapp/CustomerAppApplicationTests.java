6176c669330559af839680bcc6b062943b7ae415
Henkky <henkky888@gmail.com>
2023-04-13 15:21:01 +0700

    daily 13 April

--- CustomerApp/src/test/java/id/co/travel/customerapp/CustomerAppApplicationTests.java
+++ CustomerApp/src/test/java/id/co/travel/customerapp/CustomerAppApplicationTests.java
@@ -1,13 +1,37 @@
 package id.co.travel.customerapp;
 
+import id.co.travel.customerapp.controller.RestCustomerController;
+import id.co.travel.customerapp.model.Customer;
+import id.co.travel.customerapp.service.CustomerService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+//@SpringBootTest
+@WebMvcTest(RestCustomerController.class)
 class CustomerAppApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private CustomerService service;
 
 	@Test
-	void contextLoads() {
+	public void customerShouldReturnMessageFromService() throws Exception {
+		
 	}
 
 }
